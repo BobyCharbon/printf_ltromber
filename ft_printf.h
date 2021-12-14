34b61db2587b6cc5c0a3074f7987e7ae493432da
Boby <boby01170@gmail.com>
2021-12-14 11:08:31 +0100

    created printf.h

---
+++ ft_printf.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ludovictrombert <ludovictrombert@studen    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/14 10:06:02 by ludovictrom       #+#    #+#             */
+/*   Updated: 2021/12/14 10:49:21 by ludovictrom      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINT_H
+# define FT_PRINT_H
+#include <stdarg.h>
+
+int		ft_printf(const char*s, ...);
+
+#endif
