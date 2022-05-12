ad03ea31c218164d17f75fa85f90a91b47c8d6ce
Boby <boby01170@gmail.com>
2022-05-12 16:16:56 +0200

    created and finished exam3.c

---
+++ inter_exam3.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   inter_exam3.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ludovictrombert <ludovictrombert@studen    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/12 16:10:35 by ludovictrom       #+#    #+#             */
+/*   Updated: 2022/05/12 16:16:25 by ludovictrom      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <unistd.h>
+
+int main(int argc, char** argv)
+{
+	int used[255];
+	int i, j;
+	
+	if(argc == 3)
+	{
+		i = 0;
+		while (i < 255)
+			used[i++] = 0;
+		i = 2;
+		while (i > 0)
+		{
+			j = 0;
+			while (argv[i][j] != '\0')
+			{
+				if (i == 2 && used[(unsigned char)argv[i][j]] == '\0')
+					used[(unsigned char)argv[i][j]] = 1;
+				else if (i == 1 && used[(unsigned char)argv[i][j]] == 1)
+				{
+					write(1, &argv[i][j], 1);
+					used[(unsigned char)argv[i][j]] = 2;
+				}
+				j++;
+			}
+			i--;
+		}
+	}
+	write(1, "\n", 1);
+	return(0);
+}
+
+
+
